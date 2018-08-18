05f611aae9acd0e58f22a4d34e80cdf563fe0c08
Akerke <akerke.kass@gmail.com>
2018-08-18 16:06:32 +0600

    main function added

---
+++ get_words.py
@@ -0,0 +1,30 @@
+import sys
+
+from utilities.parser import Parser
+
+
+def clone(parser):
+    pass
+
+
+def parse(parser):
+    part_of_speech = 'verb'
+    target_folder = 'func'
+    directories = 'target_folder'
+
+    parser.parse_most_common_words(part_of_speech, target_folder, directories)
+    return parser
+
+
+def main():
+    _, action_type = sys.argv
+    parser = Parser()
+    if action_type == 'parse':
+        parse(parser)
+    elif action_type == 'clone':
+        clone(parser)
+    parser.save_results()
+
+
+if __name__=="__main__":
+    main()
