5fbc0b1bac9d882e25978c6d472b2e7338277e7d
Akerke <akerke.kass@gmail.com>
2018-08-18 17:04:07 +0600

    added more functionality to parser and parsermixin

--- utilities/mixins.py
+++ utilities/mixins.py
@@ -1,6 +1,17 @@
+import ast
+from nltk import pos_tag
+
 class ValidatorMixin:
     pass
 
 
 class PyScriptParserMixin:
-    pass
+
+    def get_trees(self, paths):
+        return None
+
+    def parse_functions(self, tree):
+        pass
+
+    def parse_words_of_given_pos(self, func_names):
+        return None
--- utilities/parser.py
+++ utilities/parser.py
@@ -31,11 +31,22 @@ class Parser(ValidatorMixin, PyScriptParserMixin):
         counted_words = collections.Counter(words)
         self.set_most_common_words(counted_words.most_common())
 
-    def get_paths_to_scripts_file(self):
-        pass
+    def get_paths_to_scripts_file(self, extension = '.py'):
+        paths = []
+        for directory in self.directories:
+            for dirname, _, filenames in os.walk(directory, topdown=True):
+                paths.extend([os.path.join(dirname, filename)
+                              for filename in filenames if filename.endswith(extension)])
+        return paths
 
     def get_words(self, paths):
-        pass
+        return
+        # content_holding_tree = self.get_trees(paths)
+        # func_names = []
+        # for tree in content_holding_tree:
+        #     func_names.extend(self.parse_functions(tree))
+        # return self.parse_words_of_given_pos(func_names)
+
 
     def set_most_common_words(self, counter):
         self.most_common_words = counter
